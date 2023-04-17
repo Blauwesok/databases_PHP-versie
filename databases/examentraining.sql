626adb8bcc4c086d59c54ad9863f8fdc4285d1d4
Blauwesok <152615@csgaugustinus.nl>
2023-04-17 10:03:47 +0000

    111

--- databases/examentraining.sql
+++ databases/examentraining.sql
@@ -1,8 +1,7 @@
 DROP DATABASE IF EXISTS `examentraining`;
-
 CREATE DATABASE `examentraining`;
-
 USE `examentraining`;
+
 CREATE TABLE `vakken` (
 `vak` varchar(16) PRIMARY KEY,
 `docentcode` varchar(3) NOT NULL,

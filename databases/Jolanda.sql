71c72308537ec901657e1501e37172f21c762a71
Blauwesok <152615@csgaugustinus.nl>
2023-04-19 09:58:16 +0000

    enter

--- databases/Jolanda.sql
+++ databases/Jolanda.sql
@@ -10,7 +10,19 @@ CREATE TABLE `haarproducten`(
    `prijs` int NOT NULL
 );
 
+CREATE TABLE `make_up_producten`(
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `Product` text NOT NULL,
+    `prijs` int NOT NULL
+);
+
 INSERT INTO `haarproducten` (`id`, `product`, `prijs`) VALUES
 (1, 'shampoo', '15'),
 (2, 'Conditioner', '20'),
 (3, 'ZilverShampoo', '12');
+
+INSERT INTO `make_up_producten`(`id`, `product`, `prijs`) VALUES
+(1, 'make up remover', '10'),
+(2, 'primer', '15'),
+(3, 'make up palette', '30')
+(4, 'make up bananenpoeder', '25')
